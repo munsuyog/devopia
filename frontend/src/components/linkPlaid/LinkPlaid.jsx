37b5048f6f3962850e7238d4a95608e38e96ba45
munsuyog <mundhesuyog@gmail.com>
2024-04-14 10:34:49 +0530

    Final Commits

--- frontend/src/components/linkPlaid/LinkPlaid.jsx
+++ frontend/src/components/linkPlaid/LinkPlaid.jsx
@@ -2,40 +2,44 @@ import {useEffect, useState} from 'react'
 import axios from 'axios';
 import {usePlaidLink} from "react-plaid-link";
 import Cookies from 'js-cookie';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 // import './App.css'
 
 axios.defaults.baseURL ="http://localhost:8000"
 
-function PlaidAuth({publicToken, token, setBankToken, setBankConnected}) {
+function PlaidAuth({publicToken}) {
   const [account, setAccount] = useState();
 
   useEffect(() => {
     async function fetchData() {
       let accessToken = await axios.post("/exchange_public_token", {public_token: publicToken});
       console.log("accessToken", accessToken.data);
-      Cookies.set('token', accessToken.data.accessToken)
       const auth = await axios.post("/auth", {access_token: accessToken.data.accessToken});
       console.log("auth data ", auth.data);
       setAccount(auth.data.numbers.ach[0]);
-    setBankConnected(true)
     }
     fetchData();
   }, []);
   return (
     <>
       {account ? (
-        <>
-          <p>Account number: {account.account}</p>
-          <p>Routing number: {account.routing}</p>
-        </>
+            <Box pt={{ base: "130px", md: "80px", xl: "80px" }} >
+        <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Text>Linked Successfully...</Text>
+        </Flex>
+    </Box>
       ) : (
-        <p>Loading...</p>
+        <Box pt={{ base: "130px", md: "80px", xl: "80px" }} >
+        <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Text>Loading...</Text>
+        </Flex>
+    </Box>
       )}
     </>
   );  
 }
 
-function LinkPlaid({setBankConnected, setBankToken}) {
+function LinkPlaid() {
   const [linkToken, setLinkToken] = useState();
   const [publicToken, setPublicToken] = useState();
   const [token, setToken] = useState(null);
@@ -53,15 +57,18 @@ function LinkPlaid({setBankConnected, setBankToken}) {
     onSuccess: (public_token, metadata) => {
       setPublicToken(public_token);
       console.log("success", public_token, metadata);
-      setToken(public_token)
-      // send public_token to server
     },
   });
 
-  return publicToken ? (<PlaidAuth publicToken={publicToken} setBankConnected={setBankConnected} setBankToken={setBankToken} token={token} />) : (
-      <button onClick={() => open()} disabled={!ready}>
-        Connect a bank account
-      </button>
+  return publicToken ? (<PlaidAuth publicToken={publicToken} />) : (
+    <Box pt={{ base: "130px", md: "80px", xl: "80px" }} >
+        <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Text>Please link your bank/demat account to retrive data:</Text>
+        <Button onClick={() => open()} disabled={!ready}>
+        Link Account
+      </Button>
+        </Flex>
+    </Box>
   );
 }
 
